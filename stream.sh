5aaca684952157a56be8679d82b567a1319adc0b
Azathoth88 <44654944+Azathoth88@users.noreply.github.com>
2024-02-03 12:51:53 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-libcamera-vid -t 0 --inline --width 1920 --height 1080 --framerate 15 -o - | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
+libcamera-vid -t 0 --inline --width 1920 --height 1080 --framerate 15 --vflip -o - | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
